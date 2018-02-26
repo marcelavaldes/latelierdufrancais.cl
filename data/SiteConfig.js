bf0963484749743d2e47af1878a2245103637598
tetsuo shima <tetsuo@500CULEBRAS.cyberpun.ga>
2018-02-26 02:15:08 -0300

    more changes to SiteConfig.

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -10,10 +10,10 @@ module.exports = {
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
   googleAnalyticsID: "UA-78034545-3", // GA tracking ID.
   //disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // Disqus shortname.
-  postDefaultCategoryID: "French", // Default category for posts.
-  userName: "Advanced User", // Username to display in the author segment.
-  userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "North Pole, Earth", // User location to display in the author segment.
+  postDefaultCategoryID: "Aprende", // Default category for posts.
+  userName: "L'atelier du Français", // Username to display in the author segment.
+  userTwitter: "latelierdufrancais", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Talca, Chile", // User location to display in the author segment.
   userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
   userDescription:
     "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
@@ -31,11 +31,11 @@ module.exports = {
     },
     {
       label: "Email",
-      url: "mailto:vagr9k@gmail.com",
+      url: "mailto:bonjour@latelierdufrancais.cl",
       iconClassName: "fa fa-envelope"
     }
   ],
-  copyright: "Copyright © 2017. Advanced User", // Copyright string for the footer of the website and RSS feed.
+  copyright: "Copyright © 2018. L'atelier du Français", // Copyright string for the footer of the website and RSS feed.
   themeColor: "#c62828", // Used for setting manifest and progress theme colors.
   backgroundColor: "#e0e0e0" // Used for setting manifest background color.
 };
