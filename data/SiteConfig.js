26742a85726358d4eda621fcb5179c8a2e121a0c
tetsuo shima <tetsuo@500CULEBRAS>
2018-04-15 15:49:27 -0300

    fixed sideConfig.js

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -6,7 +6,7 @@ module.exports = {
   siteImage: "/logos/p1.png", // Image used for OpenGraph.
   siteUrl: "https://www.latelierdufrancais.cl", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: "Somos una comunidad en torno a la lengua y cultura francesa. Estamos en Talca y San Clemente.", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "Somos una comunidad en torno a la lengua y cultura francesa. Realizamos nuestros talleres y actividades en la Región del Maule.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "148214869193038", // FB Application ID for using app insights
   googleAnalyticsID: "UA-78034545-3", // GA tracking ID.
@@ -17,7 +17,7 @@ module.exports = {
   userLocation: "Talca, Chile", // User location to display in the author segment.
   userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
   userDescription:
-    "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
+    "Somos una comunidad en torno a la lengua y cultura francesa. Realizamos nuestros talleres y actividades en la Región del Maule.", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
@@ -37,6 +37,6 @@ module.exports = {
     }
   ],
   copyright: "Copyright © 2018. L'atelier du Français", // Copyright string for the footer of the website and RSS feed.
-  themeColor: "#c62828", // Used for setting manifest and progress theme colors.
-  backgroundColor: "#e0e0e0" // Used for setting manifest background color.
+  themeColor: "#ed6a5a", // Used for setting manifest and progress theme colors.
+  backgroundColor: "##1f363d" // Used for setting manifest background color.
 };
