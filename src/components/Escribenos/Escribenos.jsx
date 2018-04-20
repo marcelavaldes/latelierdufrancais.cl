49dde3031ac19f0928192773efbc052ffc15f2f0
tetsuo shima <tetsuo@500CULEBRAS>
2018-04-20 18:56:32 -0300

    aaa

--- src/components/Escribenos/Escribenos.jsx
+++ src/components/Escribenos/Escribenos.jsx
@@ -1,33 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Form } from 'semantic-ui-react';
+import "./Escribenos.scss";
 
 export default class Escribenos extends Component {
   render() {
     return (
-      <section id="contact">
-        <div className="inner">
-          <section>
-            <form name="ahorazi" method="post" action="/merci" data-netlify="true" data-netlify-honeypot="bot-field">
-              <input type="hidden" name="bot-field" />
-              <div className="field half first">
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" required />
-              </div>
-              <div className="field half">
-                <label htmlFor="email">Email</label>
-                <input type="text" name="email" id="email" required />
-              </div>
-              <div className="field">
-                <label htmlFor="message">Message</label>
-                <textarea name="message" id="message" rows="6" required></textarea>
-              </div>
-              <ul className="actions">
-                <li><input type="submit" value="Send Message" className="special" /></li>
-                <li><input type="reset" value="Clear" /></li>
-              </ul>
-            </form>
-          </section>
-        </div>
-      </section>
+        <form name="ahorazi" method="post" action="/merci" data-netlify="true" data-netlify-honeypot="bot-field">
+          <input type="hidden" name="bot-field" />
+          <label htmlFor="name">
+            <p>
+              Nombre:
+              <input
+                type="text"
+                name="name"
+                id="name"
+                required
+              />
+            </p>
+          </label>
+          <label htmlFor="email">
+            <p>
+              Email:
+              <input
+                type="text"
+                name="email"
+                id="email"
+                required
+              />
+            </p>
+          </label>
+          <label htmlFor="message">
+            <p>
+              Message
+              <textarea
+                name="message"
+                required
+              />
+            </p>
+          </label>
+          <input type="submit" value="Send Message" className="special" />
+          <input type="reset" value="Clear" />
+        </form>
     )
   }
 }
