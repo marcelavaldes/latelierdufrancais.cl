4b96d87348ded065ed8659eaa69fa3ed52649960
tetsuo shima <tetsuo@500CULEBRAS>
2018-04-20 17:50:23 -0300

    nu form

--- src/components/Escribenos/Escribenos.jsx
+++ src/components/Escribenos/Escribenos.jsx
@@ -15,37 +15,21 @@ export default class Escribenos extends Component {
   }
 
   handleSubmit(event) {
-    console.log("ESOOOOOOOO");
+    alert('Ok, ' + this.state.value + ', te contactaremos a la brevedad.');
     event.preventDefault();
   }
 
   render() {
     return (
-      <section id="contact">
-        <div className="inner">
-          <section>
-            <form name="contact" method="post" action="#" onSubmit={this.handleSubmit} data-netlify="true" data-netlify-honeypot="bot-field">
-              <input type="hidden" name="bot-field" />
-              <div className="field half first">
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" value={this.state.value} onChange={this.handleChange} required />
-              </div>
-              <div className="field half">
-                <label htmlFor="email">Email</label>
-                <input type="text" name="email" id="email" required />
-              </div>
-              <div className="field">
-                <label htmlFor="message">Message</label>
-                <textarea name="message" id="message" rows="6" required></textarea>
-              </div>
-              <ul className="actions">
-                <li><input type="submit" value="Send Message" className="special" /></li>
-                <li><input type="reset" value="Clear" /></li>
-              </ul>
-            </form>
-          </section>
-        </div>
-      </section>
+      <form onSubmit={this.handleSubmit} name="contactos" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="bot-field" />
+        {console.log(this.state)}
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
