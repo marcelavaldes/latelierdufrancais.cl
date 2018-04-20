06807e67f6d9945fd032bd51ab185fb353cc8691
tetsuo shima <tetsuo@500CULEBRAS>
2018-04-20 18:12:28 -0300

    aer

--- src/components/Escribenos/Escribenos.jsx
+++ src/components/Escribenos/Escribenos.jsx
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
-import { Label, Header } from 'semantic-ui-react';
+import React, { Component } from 'react'
 
 export default class Escribenos extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('Ok, ' + this.state.value + ', te contactaremos a la brevedad.');
-    //event.preventDefault();
-  }
-
   render() {
     return (
-      <form onSubmit={this.handleSubmit} name="contactos" method="post" netlify="true" netlify-honeypot="bot-field">
-        <input type="hidden" name="bot-field" />
-        {console.log(this.state)}
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
+      <section id="contact">
+        <div className="inner">
+          <section>
+            <form name="ahorazi" method="post" action="/merci" data-netlify="true" data-netlify-honeypot="bot-field">
+              <input type="hidden" name="bot-field" />
+              <div className="field half first">
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" required />
+              </div>
+              <div className="field half">
+                <label htmlFor="email">Email</label>
+                <input type="text" name="email" id="email" required />
+              </div>
+              <div className="field">
+                <label htmlFor="message">Message</label>
+                <textarea name="message" id="message" rows="6" required></textarea>
+              </div>
+              <ul className="actions">
+                <li><input type="submit" value="Send Message" className="special" /></li>
+                <li><input type="reset" value="Clear" /></li>
+              </ul>
+            </form>
+          </section>
+        </div>
+      </section>
+    )
   }
 }
