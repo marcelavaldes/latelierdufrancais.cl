a135da8827abb139bd9bd6e137aae8e3d6dc7117
natural data corruption <tetsuo@cyberpun.ga>
2018-04-25 23:52:43 -0300

    formmmmmmm sui

--- src/components/Escribenos/Escribenos.jsx
+++ src/components/Escribenos/Escribenos.jsx
@@ -5,8 +5,12 @@ import "./Escribenos.scss";
 export default class Escribenos extends Component {
   render() {
     return (
-      <Form name="ahorazi" method="post" action="/merci" data-netlify="true" data-netlify-honeypot="bot-field" style={{ fontSize: "1.66em" }}>
+      <Form name="AER" method="post" action="/merci" data-netlify="true" data-netlify-honeypot="bot-field" style={{ fontSize: "1.66em" }}>
         <input type="hidden" name="bot-field" />
+        <Form.Group widths='equal'>
+          <Form.Input fluid type='text' name='name2' label='Nombre' placeholder='Simone de Beauvoir'  />
+          <Form.Input fluid type='text' name='email2' label='Correo' placeholder='simone@beauvoir.fr' />
+        </Form.Group>
         <label htmlFor="name">
           <p>
             Nombre:
