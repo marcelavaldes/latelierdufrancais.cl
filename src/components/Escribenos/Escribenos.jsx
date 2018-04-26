626eac18b32b1a5ade229311aa1846011ad603bf
natural data corruption <tetsuo@cyberpun.ga>
2018-04-26 00:14:38 -0300

    better sui form

--- src/components/Escribenos/Escribenos.jsx
+++ src/components/Escribenos/Escribenos.jsx
@@ -5,44 +5,13 @@ import "./Escribenos.scss";
 export default class Escribenos extends Component {
   render() {
     return (
-      <Form name="AER" method="post" action="/merci" data-netlify="true" data-netlify-honeypot="bot-field" style={{ fontSize: "1.66em" }}>
+      <Form name="LEGAL" method="post" action="/merci" data-netlify="true" data-netlify-honeypot="bot-field">
         <input type="hidden" name="bot-field" />
         <Form.Group widths='equal'>
-          <Form.Input fluid type='text' name='name2' label='Nombre' placeholder='Simone de Beauvoir'  />
-          <Form.Input fluid type='text' name='email2' label='Correo' placeholder='simone@beauvoir.fr' />
+          <Form.Input fluid type='text' name='name' label='Nombre' placeholder='Simone de Beauvoir' required />
+          <Form.Input fluid type='text' name='email' label='Email' placeholder='simone@beauvoir.fr' required />
         </Form.Group>
-        <label htmlFor="name">
-          <p>
-            Nombre:
-            <input
-              type="text"
-              name="name"
-              id="name"
-              required
-            />
-          </p>
-        </label>
-        <label htmlFor="email">
-          <p>
-            Email:
-            <input
-              type="text"
-              name="email"
-              id="email"
-              required
-            />
-          </p>
-        </label>
-        <label htmlFor="message">
-          <p>
-            Message
-            <textarea
-              className="notes"
-              name="message"
-              required
-            />
-          </p>
-        </label>
+        <Form.TextArea name="message" label='Mensaje' placeholder='Cuéntanos tu idea, consulta, blabla blablabla' required />
         <Button type="submit" size="large" color="red">
           Enviar
         </Button>
